5a94ff1f82cb55cdf8fc176a2f937b137e69323c
MoatasimShakhtoori <moatasim2013m@gmail.com>
2026-03-04 05:17:38 +0300

    Add dashboard guide navigation UI test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Users from './pages/Users';
 import Branches from './pages/Branches';
 import Zones from './pages/Zones';
 import BillingAccountingDemo from './pages/BillingAccountingDemo';
+import ParentCommunicationGuide from './pages/ParentCommunicationGuide';
 
 const ProtectedRoute = ({ children, roles }) => {
   const { isAuthenticated, user, loading } = useAuth();
@@ -87,6 +88,16 @@ function App() {
             element={
               <ProtectedRoute>
                 <Zones />
+              </ProtectedRoute>
+            }
+          />
+
+
+          <Route
+            path="/guides/parent-communication-step-4"
+            element={
+              <ProtectedRoute>
+                <ParentCommunicationGuide />
               </ProtectedRoute>
             }
           />
